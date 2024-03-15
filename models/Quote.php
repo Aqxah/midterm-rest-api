a851d0eaa8bfcba386b60f0fee6f2a967a642978
Tanner Legleiter <tanner.legleiter@gmail.com>
2024-03-15 00:43:51 -0500

    T1

--- models/Quote.php
+++ models/Quote.php
@@ -142,7 +142,7 @@
             categories c ON q.category_id = c.id
         WHERE 
             q.id = ?
-        LIMIT 0,1';
+        LIMIT 1 OFFSET 0';
     
         // Prepare Statement
         $stmt = $this->conn->prepare($query);
